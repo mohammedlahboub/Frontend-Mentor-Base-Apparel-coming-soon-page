59625ab992ef9220b201be2b49ae9871666f96d3
mohammedlahboub <104053591+mohammedlahboub@users.noreply.github.com>
2022-12-09 20:17:07 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        // Configure your color palette here
+        // transparent: 'transparent',
+        // current: 'currentColor',
+        'desaturated-red': 'hsl(0, 36%, 70%)',
+        'soft-red': 'hsl(0, 93%, 68%)',
+        'dark-grayish-red': 'hsl(0, 6%, 24%)',
+  
+      },
+      fontFamily: {
+        'josefin-sans': ['"Josefin Sans"', 'sans-serifcc'], //font-family: 'Josefin Sans', sans-serif;
+      },
+      backgroundImage: {
+        'bg-pattern-desktop': "url('/images/bg-pattern-desktop.svg')",
+      },
+      boxShadow: {
+        'this': '0 0.5rem 1rem 0.1rem hsl(0, 36%, 70%) ',
+      },
+    },
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      'md': '1000px',
+      // => @media (min-width: 800px) { ... }
+
+      'lg': '1440px',
+       // => @media (min-width: 1440px) { ... }
+    },
+    
+    
+
+  },
+  plugins: [],
+}
